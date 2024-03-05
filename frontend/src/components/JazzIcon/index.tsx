e2e5d20d3077d2153df5931face9960f0426d925
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-03-05 11:03:42 +0100

    Add JazzIcon

---
+++ frontend/src/components/JazzIcon/index.tsx
@@ -0,0 +1,26 @@
+import createJazzIcon from '@metamask/jazzicon'
+import classes from './index.module.css'
+import { FC, memo, useEffect, useRef } from 'react'
+import { NumberUtils } from '../../utils/number.utils.ts'
+
+interface JazzIconProps {
+  address: string
+  size: number
+}
+
+const JazzIconCmp: FC<JazzIconProps> = ({ address, size }) => {
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (ref?.current) {
+      const seed = NumberUtils.jsNumberForAddress(address)
+      const icon = createJazzIcon(size, seed)
+
+      ref.current?.replaceChildren(icon)
+    }
+  }, [size, ref, address])
+
+  return <div ref={ref} style={{ width: size, height: size }} className={classes.jazzIcon} />
+}
+
+export const JazzIcon = memo(JazzIconCmp)
