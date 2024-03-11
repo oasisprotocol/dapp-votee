a0ec84bc7f2a14e23f5b149530d7e743f8dfa1a3
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-03-11 13:49:23 +0100

    Add vote initial flow

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -20,6 +20,38 @@ export const NETWORKS: Map<bigint, NetworkConfiguration> = new Map([
   ],
 ])
 
+export const MAX_GAS_LIMIT = 100000n
+
+interface PollChoice {
+  name: string
+  description: string
+  imagePath: string
+}
+
+/**
+ * This array indexes correspond to the matching choiceId of the poll
+ * Desert Owl = 0
+ * Capybara = 1
+ * Fennec Fox = 2
+ */
+export const POLL_CHOICES: readonly PollChoice[] = Object.freeze([
+  {
+    name: 'Desert Owl',
+    description: 'Lorem ipsum dolor sit amet. A repellendus illo.',
+    imagePath: 'https://fakeimg.pl/182x175',
+  },
+  {
+    name: 'Capybara',
+    description: 'Lorem ipsum dolor sit amet. A repellendus illo.',
+    imagePath: 'https://fakeimg.pl/182x175',
+  },
+  {
+    name: 'Fennec Fox',
+    description: 'Lorem ipsum dolor sit amet. A repellendus illo.',
+    imagePath: 'https://fakeimg.pl/182x175',
+  },
+])
+
 export const METAMASK_HOME_PAGE = 'https://metamask.io/'
 
 const {
--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,98 +1,114 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { CaretRightIcon } from '../../components/icons/CaretRightIcon.tsx'
 import { Button } from '../../components/Button'
 import { Card } from '../../components/Card'
 import classes from './index.module.css'
 import { MascotCard } from '../../components/MascotCard'
+import { POLL_CHOICES } from '../../constants/config.ts'
+import { useWeb3 } from '../../hooks/useWeb3.ts'
 import { Alert } from '../../components/Alert'
+import { StringUtils } from '../../utils/string.utils.ts'
 
 export const HomePage: FC = () => {
+  const {
+    state: { isConnected },
+    vote,
+  } = useWeb3()
+
+  const [selectedChoice, setSelectedChoice] = useState<0 | 1 | 2 | null>(null)
+  const [pageStatus, setPageStatus] = useState<'loading' | 'error' | 'success' | null>(null)
+  const [error, setError] = useState('')
+
+  const handleSelectChoice = (choice: 0 | 1 | 2) => {
+    setSelectedChoice(choice)
+  }
+
+  const handleVote = async () => {
+    if (selectedChoice === null) return
+
+    setPageStatus('loading')
+
+    try {
+      await vote(selectedChoice)
+
+      setPageStatus('success')
+    } catch (ex) {
+      console.error(ex)
+      setError((ex as Error).message ?? JSON.stringify(ex))
+
+      setPageStatus('error')
+    }
+  }
+
   return (
-    <div>
-      <Alert type="insufficient-balance">
-        Please note there is a 100 ROSE threshold in order to cast your vote.
-      </Alert>
-      <br />
-      <br />
-      <Alert type="loading" actions={<span>Submitting vote...</span>}>
-        Once you confirm this vote you will not be able to cancel it.
-      </Alert>
-      <br />
-      <br />
-      <Alert
-        type="success"
-        actions={
-          <span>
-            Your vote has successfully submitted.
+    <>
+      {pageStatus === 'loading' && (
+        <Alert type="loading" actions={<span>Submitting vote...</span>}>
+          Once you confirm this vote you will not be able to cancel it.
+        </Alert>
+      )}
+      {pageStatus === 'error' && error && <Alert type="error">{error}</Alert>}
+      {pageStatus === 'success' && (
+        <Alert
+          type="success"
+          actions={
+            <span>
+              Your vote has successfully submitted.
+              <br />
+              Thank you for your participation.
+            </span>
+          }
+        />
+      )}
+      {pageStatus === null && (
+        <Card>
+          <p className={classes.cardHeaderText}>
+            Select your preferred mascot option. Once you confirm this vote you will not be able to cancel it.
+            Read more about the campaign on our website.
+          </p>
+          <div className={classes.mascotCards}>
+            {POLL_CHOICES.map(({ name, description, imagePath }, choiceId) => (
+              <MascotCard
+                key={name}
+                title={name}
+                description={description}
+                image={<img alt={name} src={imagePath} />}
+                selected={choiceId === selectedChoice}
+                actions={
+                  <div className={classes.mascotCardActions}>
+                    <Button
+                      variant="outline"
+                      size="small"
+                      color="secondary"
+                      onClick={() => handleSelectChoice(choiceId as 0 | 1 | 2)}
+                    >
+                      Select
+                    </Button>
+                  </div>
+                }
+              />
+            ))}
+          </div>
+          <div className={classes.cardAction}>
+            <Button disabled={selectedChoice === null || !isConnected} onClick={handleVote}>
+              <label
+                className={StringUtils.clsx(
+                  selectedChoice === null || !isConnected ? classes.voteBtnDisabled : classes.voteBtnLabel
+                )}
+              >
+                {(isConnected || selectedChoice === null) && <>Continue</>}
+                {!isConnected && selectedChoice !== null && <>Wallet not connected</>}
+                <CaretRightIcon />
+              </label>
+            </Button>
+          </div>
+          <p className={classes.cardFooterText}>
+            Please note there is a 100 ROSE threshold in order to cast your vote.
             <br />
-            Thank you for your participation.
-          </span>
-        }
-      />
-      <br />
-      <br />
-      <Alert type="error">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Ipsum
-      </Alert>
-      <br />
-      <br />
-      <Card>
-        <p className={classes.cardHeaderText}>
-          Select your preferred mascot option. Once you confirm this vote you will not be able to cancel it.
-          Read more about the campaign on our website.
-        </p>
-        <div className={classes.mascotCards}>
-          <MascotCard
-            title="Desert Fox"
-            description="Lorem ipsum dolor sit amet. A repellendus illo."
-            image={<img alt="Desert Fox" src="https://fakeimg.pl/182x175" />}
-            actions={
-              <div className={classes.mascotCardActions}>
-                <Button variant="outline" size="small" color="secondary">
-                  Select
-                </Button>
-              </div>
-            }
-          />
-          <MascotCard
-            title="Capybara"
-            description="Lorem ipsum dolor sit amet. A repellendus illo."
-            image={<img alt="Capybara" src="https://fakeimg.pl/182x175" />}
-            actions={
-              <div className={classes.mascotCardActions}>
-                <Button variant="outline" size="small" color="secondary">
-                  Select
-                </Button>
-              </div>
-            }
-          />
-          <MascotCard
-            title="Camel"
-            description="Lorem ipsum dolor sit amet. A repellendus illo."
-            image={<img alt="Camel" src="https://fakeimg.pl/182x175" />}
-            actions={
-              <div className={classes.mascotCardActions}>
-                <Button variant="outline" size="small" color="secondary">
-                  Select
-                </Button>
-              </div>
-            }
-          />
-        </div>
-        <div className={classes.cardAction}>
-          <Button>
-            <label>
-              Continue
-              <CaretRightIcon />
-            </label>
-          </Button>
-        </div>
-        <p className={classes.cardFooterText}>
-          Please note there is a 100 ROSE threshold in order to cast your vote.
-          <br />
-          Poll closes on March 31st, 2024 at 00:00 CET.
-        </p>
-      </Card>
-    </div>
+            Poll closes on March 31st, 2024 at 00:00 CET.
+          </p>
+        </Card>
+      )}
+    </>
   )
 }
