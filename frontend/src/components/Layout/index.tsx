b6a740fdf9cf0b1f8782075ed0e712b197c79203
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-03-11 11:10:12 +0100

    Add getPoll contact call
    - prepare canVoteOnPoll & vote contract calls

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,10 +1,47 @@
-import { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { FC, useEffect, useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import classes from './index.module.css'
 import { LogoIcon } from '../icons/LogoIcon'
 import { ConnectWallet } from '../ConnectWallet'
+import { useWeb3 } from '../../hooks/useWeb3.ts'
+import { Alert } from '../Alert'
 
 export const Layout: FC = () => {
+  const {
+    state: { isVoidSignerConnected },
+    getPoll,
+  } = useWeb3()
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (!isVoidSignerConnected) return
+
+    const init = async () => {
+      const {
+        active,
+        params: { closeTimestamp, numChoices },
+      } = await getPoll()
+
+      if (numChoices !== 3n) {
+        console.warn('[numChoices] Unexpected number of poll choices, this dApp may not behave as expected!')
+      }
+
+      setIsLoading(false)
+
+      if (!active) {
+        navigate('/results')
+        console.log('closeTimestamp', closeTimestamp)
+      } else {
+        navigate('/')
+      }
+    }
+
+    init()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isVoidSignerConnected])
+
   return (
     <div className={classes.layout}>
       <main className={classes.main}>
@@ -16,7 +53,10 @@ export const Layout: FC = () => {
           <h1>Oasis Mascot</h1>
         </section>
         <section>
-          <Outlet />
+          {isLoading && (
+            <Alert headerText="Please wait" type="loading" actions={<span>Fetching poll...</span>} />
+          )}
+          {!isLoading && <Outlet />}
         </section>
       </main>
     </div>
--- frontend/src/providers/Web3Context.ts
+++ frontend/src/providers/Web3Context.ts
@@ -1,14 +1,19 @@
 import { createContext } from 'react'
 import * as sapphire from '@oasisprotocol/sapphire-paratime'
-import { BrowserProvider, TransactionResponse } from 'ethers'
+import { BigNumberish, BrowserProvider, ContractTransactionResponse, TransactionResponse } from 'ethers'
+import { type PollManager } from '@oasisprotocol/dapp-voting-backend/src/contracts'
+import { DefaultReturnType } from '@oasisprotocol/dapp-voting-backend/src/contracts/common.ts'
 
 export interface Web3ProviderState {
   isConnected: boolean
+  isVoidSignerConnected: boolean
   ethProvider: BrowserProvider | null
   sapphireEthProvider: (BrowserProvider & sapphire.SapphireAnnex) | null
   account: string | null
   explorerBaseUrl: string | null
   networkName: string | null
+  pollManager: PollManager | null
+  pollManagerVoidSigner: PollManager | null
 }
 
 export interface Web3ProviderContext {
@@ -18,6 +23,19 @@ export interface Web3ProviderContext {
   getBalance: () => Promise<bigint>
   getTransaction: (txHash: string) => Promise<TransactionResponse | null>
   isProviderAvailable: () => Promise<boolean>
+  getPoll: () => Promise<
+    DefaultReturnType<
+      [
+        [boolean, bigint, PollManager.ProposalParamsStructOutput] & {
+          active: boolean
+          topChoice: bigint
+          params: PollManager.ProposalParamsStructOutput
+        },
+      ]
+    >
+  >
+  canVoteOnPoll: () => Promise<boolean>
+  vote: (choiceId: BigNumberish) => Promise<ContractTransactionResponse>
 }
 
 export const Web3Context = createContext<Web3ProviderContext>({} as Web3ProviderContext)
