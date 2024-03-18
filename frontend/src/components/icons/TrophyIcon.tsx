f24a5cd8f8b1f5d1d44428b730b4511dbf3dc8bf
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-03-18 13:32:04 +0100

    Mobile ResultsPage and its contents design
    - Adjust HomePage footer text

--- frontend/src/components/icons/TrophyIcon.tsx
+++ frontend/src/components/icons/TrophyIcon.tsx
@@ -3,9 +3,16 @@
 import { FC } from 'react'
 import TrophySvg from '@phosphor-icons/core/assets/fill/trophy-fill.svg?react'
 import { Icon } from '../Icon'
+import { IconSize } from '../../types'
 
-export const TrophyIcon: FC = () => (
-  <Icon size="small">
+interface Props {
+  size?: IconSize
+  width?: number | string
+  height?: number | string
+}
+
+export const TrophyIcon: FC<Props> = ({ size, width, height }) => (
+  <Icon size={size} width={width} height={height}>
     <TrophySvg />
   </Icon>
 )
