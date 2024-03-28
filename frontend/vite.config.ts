ae02575442c772a8abcbc9ffd4b6363087537514
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-03-28 05:56:10 +0100

    Add rollup-plugin-visualizer plugin
    - add splitVendorChunkPlugin
    - add visualize:bundle command using vite-bundle-vizualizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption, splitVendorChunkPlugin } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), splitVendorChunkPlugin(), visualizer() as PluginOption],
 })
